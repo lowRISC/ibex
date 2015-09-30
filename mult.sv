12112374940bdcb99ec8223ed038ee42dcf96438
Sven Stucki <git@svenstucki.ch>
2015-09-30 18:40:56 +0200

    Remove vectorial stuff from multiplier

--- mult.sv
+++ mult.sv
@@ -24,13 +24,10 @@
 //                 selection. There are no flags for multiplications anymore! //
 //                                                                            //
 //                                                                            //
-//                                                                            //
-//                                                                            //
-//                                                                            //
 ////////////////////////////////////////////////////////////////////////////////
 
-
 `include "defines.sv"
+
 
 module mult
 (
@@ -46,14 +43,10 @@ module mult
    output logic [31:0]  result_o
 );
 
-  logic [31:0]        result;
+  logic [31:0] op_a_sel;
+  logic [31:0] op_b_sel;
+  logic [31:0] mac_int;
 
-  logic [31:0]        op_a_sel;
-  logic [31:0]        op_b_sel;
-  logic [31:0]        mac_int;
-
-
-  assign mac_int = (mac_en_i == 1'b1) ? mac_i : 32'b0;
 
   // this block performs the subword selection and sign extensions
   always_comb
@@ -78,32 +71,8 @@ module mult
     end
   end
 
-
-  always_comb
-  begin
-    case(vector_mode_i)
-      `VEC_MODE16:
-      begin
-        result[15: 0] = mac_int[15: 0] + op_a_sel[15: 0] * op_b_sel[15: 0];
-        result[31:16] = mac_int[31:16] + op_a_sel[31:16] * op_b_sel[31:16];
-      end
-
-      `VEC_MODE8:
-      begin
-        result[ 7: 0] = mac_int[ 7: 0] + op_a_sel[ 7: 0] * op_b_sel[ 7: 0];
-        result[15: 8] = mac_int[15: 8] + op_a_sel[15: 8] * op_b_sel[15: 8];
-        result[23:16] = mac_int[23:16] + op_a_sel[23:16] * op_b_sel[23:16];
-        result[31:24] = mac_int[31:24] + op_a_sel[31:24] * op_b_sel[31:24];
-      end
-
-      default: // VEC_MODE32, VEC_MODE216
-      begin
-        result[31: 0] = mac_int + op_a_sel * op_b_sel;
-      end
-    endcase; // case (vec_mode_i)
-  end
-
-  assign result_o   = result[31:0];
+  assign mac_int  = (mac_en_i == 1'b1) ? mac_i : 32'b0;
+  assign result_o = mac_int + op_a_sel * op_b_sel;
 
 endmodule // mult
 
