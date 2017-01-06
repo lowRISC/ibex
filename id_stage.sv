537fbae947826ab3f328e6652fdb2ff185e84b83
Markus Wegmann <atokulus@gmail.com>
2017-01-06 12:43:21 +0100

    Fix last

--- id_stage.sv
+++ id_stage.sv
@@ -1614,7 +1614,7 @@ always_ff @(posedge clk, negedge rst_n)
     if(~rst_n) begin
       alu_req_ex_o <= 0;
     end else begin
-      alu_req_ex_o <= valid_id;
+      alu_req_ex_o <= id_valid_o;
     end
   end
   `endif
