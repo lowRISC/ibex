b8c150747a57a87fde6f1293654b4abd9e4c4d09
Pirmin Vogel <vogelpi@lowrisc.org>
2019-05-03 10:54:07 +0100

    Remove unused opcodes

--- rtl/ibex_defines.sv
+++ rtl/ibex_defines.sv
@@ -48,12 +48,6 @@ parameter ALU_OP_WIDTH = 6;
 
 parameter ALU_ADD   = 6'b011000;
 parameter ALU_SUB   = 6'b011001;
-parameter ALU_ADDU  = 6'b011010;
-parameter ALU_SUBU  = 6'b011011;
-parameter ALU_ADDR  = 6'b011100;
-parameter ALU_SUBR  = 6'b011101;
-parameter ALU_ADDUR = 6'b011110;
-parameter ALU_SUBUR = 6'b011111;
 
 parameter ALU_XOR   = 6'b101111;
 parameter ALU_OR    = 6'b101110;
@@ -62,25 +56,7 @@ parameter ALU_AND   = 6'b010101;
 // Shifts
 parameter ALU_SRA   = 6'b100100;
 parameter ALU_SRL   = 6'b100101;
-parameter ALU_ROR   = 6'b100110;
 parameter ALU_SLL   = 6'b100111;
-
-// bit manipulation
-parameter ALU_BEXT  = 6'b101000;
-parameter ALU_BEXTU = 6'b101001;
-parameter ALU_BINS  = 6'b101010;
-parameter ALU_BCLR  = 6'b101011;
-parameter ALU_BSET  = 6'b101100;
-
-// Bit counting
-parameter ALU_FF1   = 6'b110110;
-parameter ALU_FL1   = 6'b110111;
-parameter ALU_CNT   = 6'b110100;
-parameter ALU_CLB   = 6'b110101;
-
-// Sign-/zero-extensions
-parameter ALU_EXTS  = 6'b111110;
-parameter ALU_EXT   = 6'b111111;
 
 // Comparisons
 parameter ALU_LT    = 6'b000000;
@@ -100,41 +76,10 @@ parameter ALU_SLTU  = 6'b000011;
 parameter ALU_SLET  = 6'b000110;
 parameter ALU_SLETU = 6'b000111;
 
-// Absolute value
-parameter ALU_ABS   = 6'b010100;
-parameter ALU_CLIP  = 6'b010110;
-parameter ALU_CLIPU = 6'b010111;
-
-// Insert/extract
-parameter ALU_INS   = 6'b101101;
-
-// min/max
-parameter ALU_MIN   = 6'b010000;
-parameter ALU_MINU  = 6'b010001;
-parameter ALU_MAX   = 6'b010010;
-parameter ALU_MAXU  = 6'b010011;
-
-// div/rem
-parameter ALU_DIVU  = 6'b110000; // bit 0 is used for signed mode, bit 1 is used for remdiv
-parameter ALU_DIV   = 6'b110001; // bit 0 is used for signed mode, bit 1 is used for remdiv
-parameter ALU_REMU  = 6'b110010; // bit 0 is used for signed mode, bit 1 is used for remdiv
-parameter ALU_REM   = 6'b110011; // bit 0 is used for signed mode, bit 1 is used for remdiv
-
-parameter ALU_SHUF  = 6'b111010;
-parameter ALU_SHUF2 = 6'b111011;
-parameter ALU_PCKLO = 6'b111000;
-parameter ALU_PCKHI = 6'b111001;
-
-
 parameter MD_OP_MULL  = 2'b00;
 parameter MD_OP_MULH  = 2'b01;
 parameter MD_OP_DIV   = 2'b10;
 parameter MD_OP_REM   = 2'b11;
-
-// vector modes
-parameter VEC_MODE32 = 2'b00;
-parameter VEC_MODE16 = 2'b10;
-parameter VEC_MODE8  = 2'b11;
 
 
 //////////////////////////////////
@@ -146,18 +91,6 @@ parameter CSR_OP_NONE  = 2'b00;
 parameter CSR_OP_WRITE = 2'b01;
 parameter CSR_OP_SET   = 2'b10;
 parameter CSR_OP_CLEAR = 2'b11;
-
-
-// SPR for debugger, not accessible by CPU
-parameter SP_DVR0       = 16'h3000;
-parameter SP_DCR0       = 16'h3008;
-parameter SP_DMR1       = 16'h3010;
-parameter SP_DMR2       = 16'h3011;
-
-parameter SP_DVR_MSB = 8'h00;
-parameter SP_DCR_MSB = 8'h01;
-parameter SP_DMR_MSB = 8'h02;
-parameter SP_DSR_MSB = 8'h04;
 
 // Privileged mode
 typedef enum logic[1:0] {
@@ -180,16 +113,12 @@ typedef enum logic[3:0] {
 
 // forwarding operand mux
 parameter SEL_REGFILE      = 2'b00;
-parameter SEL_FW_EX        = 2'b01;
-parameter SEL_FW_WB        = 2'b10;
 parameter SEL_MISALIGNED   = 2'b11;
 
 // operand a selection
 parameter OP_A_REGA_OR_FWD = 3'b000;
 parameter OP_A_CURRPC      = 3'b001;
 parameter OP_A_IMM         = 3'b010;
-parameter OP_A_REGB_OR_FWD = 3'b011;
-parameter OP_A_REGC_OR_FWD = 3'b100;
 
 // immediate a selection
 parameter IMMA_Z      = 1'b0;
@@ -197,11 +126,7 @@ parameter IMMA_ZERO   = 1'b1;
 
 // operand b selection
 parameter OP_B_REGB_OR_FWD = 3'b000;
-parameter OP_B_REGC_OR_FWD = 3'b001;
 parameter OP_B_IMM         = 3'b010;
-parameter OP_B_REGA_OR_FWD = 3'b011;
-parameter OP_B_BMASK       = 3'b100;
-parameter OP_B_ZERO        = 3'b101;
 
 // immediate b selection
 parameter IMMB_I      = 4'b0000;
@@ -212,26 +137,9 @@ parameter IMMB_S2     = 4'b0100;
 parameter IMMB_S3     = 4'b0101;
 parameter IMMB_VS     = 4'b0110;
 parameter IMMB_VU     = 4'b0111;
-parameter IMMB_SHUF   = 4'b1000;
-parameter IMMB_CLIP   = 4'b1001;
 parameter IMMB_BI     = 4'b1011;
 parameter IMMB_UJ     = 4'b1100;
 parameter IMMB_SB     = 4'b1101;
-
-// bit mask selection
-parameter BMASK_A_ZERO = 1'b0;
-parameter BMASK_A_S3   = 1'b1;
-
-parameter BMASK_B_S2   = 2'b00;
-parameter BMASK_B_S3   = 2'b01;
-parameter BMASK_B_ZERO = 2'b10;
-parameter BMASK_B_ONE  = 2'b11;
-
-parameter BMASK_A_REG  = 1'b0;
-parameter BMASK_A_IMM  = 1'b1;
-parameter BMASK_B_REG  = 1'b0;
-parameter BMASK_B_IMM  = 1'b1;
-
 
 
 //////////////
@@ -270,7 +178,6 @@ parameter EXC_CAUSE_ECALL_MMODE  = 6'h0B;
 parameter EXC_OFF_RST        = 8'h80;
 parameter EXC_OFF_ILLINSN    = 8'h84;
 parameter EXC_OFF_ECALL      = 8'h88;
-parameter EXC_OFF_LSUERR     = 8'h8c;
 parameter EXC_OFF_BREAKPOINT = 8'h90;
 
 // Debug Cause
@@ -278,19 +185,6 @@ parameter DBG_CAUSE_EBREAK     = 3'h1;
 parameter DBG_CAUSE_TRIGGER    = 3'h2;
 parameter DBG_CAUSE_HALTREQ    = 3'h3;
 parameter DBG_CAUSE_STEP       = 3'h4;
-parameter DBG_CAUSE_RSTHALTREQ = 3'h5;
-
-// Debug module
-parameter DBG_SETS_W = 6;
-
-parameter DBG_SETS_IRQ    = 5;
-parameter DBG_SETS_ECALL  = 4;
-parameter DBG_SETS_EILL   = 3;
-parameter DBG_SETS_ELSU   = 2;
-parameter DBG_SETS_EBRK   = 1;
-parameter DBG_SETS_SSTE   = 0;
-
-parameter DBG_CAUSE_HALT   = 6'h1F;
 
 // CSRs
 parameter CSR_MSTATUS        = 12'h300;
