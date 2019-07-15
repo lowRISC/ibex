83435f631c38f90f1f7a093ac59e6d1364b7fc91
{“Tao <taliu@google.com>
2019-07-15 14:55:51 -0700

    Update google_riscv-dv to 2e5a401
    Update code from upstream repository https://github.com/google/riscv-
    dv to revision 2e5a40145a367ac3b04f78fee02c5011022719fd
    
    * Merge pull request #36 from google/dev (taoliug)
    * Add basic debug mode support (Tao Liu)

--- vendor/google_riscv-dv/setting/riscv_core_setting.sv
+++ vendor/google_riscv-dv/setting/riscv_core_setting.sv
@@ -32,6 +32,9 @@ riscv_instr_name_t unsupported_instr[];
 // ISA supported by the processor
 riscv_instr_group_t supported_isa[$] = {RV32I, RV32M, RV64I, RV64M, RV32C, RV64C};
 
+// Debug mode support
+bit support_debug_mode = 0;
+
 // Support delegate trap to user mode
 bit support_umode_trap = 0;
 
