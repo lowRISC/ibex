eadcf326d50439dcce59bc832b06f4bb14c98e90
Pirmin Vogel <vogelpi@lowrisc.org>
2019-07-24 14:55:34 +0100

    Remove legacy interrupt controller
    This removal got partially lost while rebasing for PR #149.

--- dv/uvm/ibex_dv.f
+++ dv/uvm/ibex_dv.f
@@ -14,7 +14,6 @@ ${PRJ_DIR}/ibex/rtl/ibex_compressed_decoder.sv
 ${PRJ_DIR}/ibex/rtl/ibex_controller.sv
 ${PRJ_DIR}/ibex/rtl/ibex_cs_registers.sv
 ${PRJ_DIR}/ibex/rtl/ibex_decoder.sv
-${PRJ_DIR}/ibex/rtl/ibex_int_controller.sv
 ${PRJ_DIR}/ibex/rtl/ibex_ex_block.sv
 ${PRJ_DIR}/ibex/rtl/ibex_id_stage.sv
 ${PRJ_DIR}/ibex/rtl/ibex_if_stage.sv
