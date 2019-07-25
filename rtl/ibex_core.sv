81d8fb7a64fb6cc062af6a96345563a9abbeca90
Pirmin Vogel <vogelpi@iis.ee.ethz.ch>
2019-07-25 16:50:50 +0200

    Switch to `cluster_clock_gating`
    This commit replaces the generic `prim_clock_gating` cell with the
    `cluster_clock_gating` cell used in PULPissimo.

--- rtl/ibex_core.sv
+++ rtl/ibex_core.sv
@@ -278,7 +278,7 @@ module ibex_core #(
   // main clock gate of the core
   // generates all clocks except the one for the debug unit which is
   // independent
-  prim_clock_gating core_clock_gate_i (
+  cluster_clock_gating core_clock_gate_i (
       .clk_i     ( clk_i           ),
       .en_i      ( clock_en        ),
       .test_en_i ( test_en_i       ),
