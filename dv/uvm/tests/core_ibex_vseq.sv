3bc83365ef1d650f0bbf3e508d5c8d450c8a594b
udinator <udij@google.com>
2019-08-20 11:03:15 -0700

    Add more debug tests (#251)

--- dv/uvm/tests/core_ibex_vseq.sv
+++ dv/uvm/tests/core_ibex_vseq.sv
@@ -8,12 +8,13 @@
 
 class core_ibex_vseq extends uvm_sequence;
 
-  ibex_mem_intf_slave_seq    instr_intf_seq;
-  ibex_mem_intf_slave_seq    data_intf_seq;
-  mem_model_pkg::mem_model   mem;
-  irq_seq                    irq_seq_h;
-  debug_seq                  debug_seq_h;
-  core_ibex_env_cfg          cfg;
+  ibex_mem_intf_slave_seq                       instr_intf_seq;
+  ibex_mem_intf_slave_seq                       data_intf_seq;
+  mem_model_pkg::mem_model                      mem;
+  irq_seq                                       irq_seq_h;
+  debug_seq                                     debug_seq_h;
+  core_ibex_env_cfg                             cfg;
+  bit[ibex_mem_intf_agent_pkg::DATA_WIDTH-1:0]  data;
 
   `uvm_object_utils(core_ibex_vseq)
   `uvm_declare_p_sequencer(core_ibex_vseqr)
@@ -22,20 +23,19 @@ class core_ibex_vseq extends uvm_sequence;
   virtual task body();
     instr_intf_seq = ibex_mem_intf_slave_seq::type_id::create("instr_intf_seq");
     data_intf_seq  = ibex_mem_intf_slave_seq::type_id::create("data_intf_seq");
+    if (cfg.enable_irq_seq) begin
+      irq_seq_h = irq_seq::type_id::create("irq_seq_h");
+    end
+    if (cfg.enable_debug_seq) begin
+      debug_seq_h = debug_seq::type_id::create("debug_seq_h");
+    end
     instr_intf_seq.m_mem = mem;
     data_intf_seq.m_mem  = mem;
+
 
     fork
       instr_intf_seq.start(p_sequencer.instr_if_seqr);
       data_intf_seq.start(p_sequencer.data_if_seqr);
-      if (cfg.enable_irq_seq) begin
-        irq_seq_h = irq_seq::type_id::create("irq_seq_h");
-        irq_seq_h.start(p_sequencer.irq_seqr);
-      end
-      if (cfg.enable_debug_seq) begin
-        debug_seq_h = debug_seq::type_id::create("debug_seq_h");
-        debug_seq_h.start(null);
-      end
     join_none
   endtask
 
@@ -46,6 +46,14 @@ class core_ibex_vseq extends uvm_sequence;
     if (cfg.enable_debug_seq) begin
       debug_seq_h.stop();
     end
+  endtask
+
+  virtual task start_debug_seq();
+    debug_seq_h.start(null);
+  endtask
+
+  virtual task start_irq_seq();
+    irq_seq_h.start(p_sequencer.irq_seqr);
   endtask
 
 endclass
