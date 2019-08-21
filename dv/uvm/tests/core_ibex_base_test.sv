f0ffef0b5293d18f25b1dbde672095320ad00b53
Udi <udij@google.com>
2019-08-21 14:00:02 -0700

    Update interrupt mode, add debug mode WFI test

--- dv/uvm/tests/core_ibex_base_test.sv
+++ dv/uvm/tests/core_ibex_base_test.sv
@@ -8,17 +8,16 @@ class core_ibex_base_test extends uvm_test;
   core_ibex_env_cfg                               cfg;
   virtual clk_if                                  clk_vif;
   virtual core_ibex_dut_probe_if                  dut_vif;
-  virtual ibex_mem_intf                           dmem_vif;
   mem_model_pkg::mem_model                        mem;
   core_ibex_vseq                                  vseq;
-  bit                                             enable_irq_seq;
-  bit                                             enable_debug_seq;
   irq_seq                                         irq_seq_h;
-  int unsigned                                    timeout_in_cycles = 2000000;
+  int unsigned                                    timeout_in_cycles = 3000000;
   // If no signature_addr handshake functionality is desired between the
   // testbench and the generated code, the test will wait for the specified
   // number of cycles before starting stimulus sequences (irq and debug)
   int unsigned                                    stimulus_delay = 800;
+  bit[ibex_mem_intf_agent_pkg::DATA_WIDTH]        signature_data_q[$];
+  bit[ibex_mem_intf_agent_pkg::DATA_WIDTH]        signature_data;
   uvm_tlm_analysis_fifo #(ibex_mem_intf_seq_item) item_collected_port;
 
   `uvm_component_utils(core_ibex_base_test)
@@ -39,9 +38,6 @@ class core_ibex_base_test extends uvm_test;
     end
     if (!uvm_config_db#(virtual core_ibex_dut_probe_if)::get(null, "", "dut_if", dut_vif)) begin
       `uvm_fatal(get_full_name(), "Cannot get dut_if")
-    end
-    if (!uvm_config_db#(virtual ibex_mem_intf)::get(null, "*data_if_slave*", "vif", dmem_vif)) begin
-      `uvm_fatal(get_full_name(), "Cannot get dmem_vif")
     end
     env = core_ibex_env::type_id::create("env", this);
     cfg = core_ibex_env_cfg::type_id::create("cfg", this);
@@ -64,7 +60,7 @@ class core_ibex_base_test extends uvm_test;
     clk_vif.wait_clks(100);
     load_binary_to_mem();
     dut_vif.fetch_enable = 1'b1;
-    vseq.start(env.vseqr);
+    send_stimulus();
     wait_for_test_done();
     phase.drop_objection(this);
   endtask
@@ -72,6 +68,10 @@ class core_ibex_base_test extends uvm_test;
   virtual function void report_phase(uvm_phase phase);
     super.report_phase(phase);
   endfunction
+
+  virtual task send_stimulus();
+    vseq.start(env.vseqr);
+  endtask
 
   function void load_binary_to_mem();
     string      bin;
@@ -115,11 +115,10 @@ class core_ibex_base_test extends uvm_test;
     join_any
   endtask
 
+
   virtual task wait_for_mem_txn(input bit[ibex_mem_intf_agent_pkg::ADDR_WIDTH-1:0] ref_addr,
-                                input signature_type_t ref_type,
-                                output bit[ibex_mem_intf_agent_pkg::DATA_WIDTH-1:0] data[$]);
+                                input signature_type_t ref_type);
     ibex_mem_intf_seq_item mem_txn;
-    bit[ibex_mem_intf_agent_pkg::DATA_WIDTH-1:0] signature_data;
     forever begin
       // The first write to this address is guaranteed to contain the
       // signature type in bits [7:0]
@@ -127,11 +126,14 @@ class core_ibex_base_test extends uvm_test;
       if (mem_txn.addr == ref_addr && mem_txn.data[7:0] === ref_type && mem_txn.read_write == WRITE) begin
         signature_data = mem_txn.data;
         case (ref_type)
+          // The very first write to the signature address in every test is
+          // guaranteed to be a write of CORE_STATUS, indicating the
+          // INITIALIZED state
           CORE_STATUS: begin
-            data.push_back(signature_data >> 8);
+            signature_data_q.push_back(signature_data >> 8);
           end
           TEST_RESULT: begin
-            data.push_back(signature_data >> 8);
+            signature_data_q.push_back(signature_data >> 8);
           end
           // The next 32 writes to the address are guaranteed to be a dump of
           // all GPRs
@@ -140,17 +142,17 @@ class core_ibex_base_test extends uvm_test;
               do begin
                 item_collected_port.get(mem_txn);
               end while(!(mem_txn.addr == ref_addr && mem_txn.read_write == WRITE));
-              data.push_back(mem_txn.data);
+              signature_data_q.push_back(mem_txn.data);
             end
           end
           // The next write to this address is guaranteed to be the data held
           // in the CSR
           WRITE_CSR: begin
-            data.push_back(signature_data >> 8);
+            signature_data_q.push_back(signature_data >> 8);
             do begin
               item_collected_port.get(mem_txn);
             end while (!(mem_txn.addr == ref_addr && mem_txn.read_write == WRITE));
-            data.push_back(mem_txn.data);
+            signature_data_q.push_back(mem_txn.data);
           end
           default: begin
             `uvm_fatal(`gfn, $sformatf("The data 0x%0h written to the signature address is formatted incorrectly.", signature_data))
@@ -159,6 +161,25 @@ class core_ibex_base_test extends uvm_test;
         return;
       end
     end
+  endtask
+
+  // API of various tasks wrapping wait_for_mem_txn, for various common
+  // functionalities that might be needed for verification purposes.
+  // Will be expanded as needed.
+
+  virtual task check_next_core_status(core_status_t core_status, error_msg="");
+    wait_for_mem_txn(cfg.signature_addr, CORE_STATUS);
+    signature_data = signature_data_q.pop_front();
+    if (signature_data != core_status) begin
+      `uvm_error(`gfn, error_msg)
+    end
+  endtask
+
+  virtual task wait_for_core_status(core_status_t core_status);
+    do begin
+      wait_for_mem_txn(cfg.signature_addr, CORE_STATUS);
+      signature_data = signature_data_q.pop_front();
+    end while (signature_data != core_status);
   endtask
 
 endclass
