9311b25fdbbd64bf62b7bf4e2e02fbfd40d2ea01
udinator <udij@google.com>
2019-08-21 11:00:17 -0700

    Consolidate some debug generation options, and make the signature_addr handshake optional (#253)

--- dv/uvm/tests/core_ibex_test_lib.sv
+++ dv/uvm/tests/core_ibex_test_lib.sv
@@ -37,8 +37,7 @@ endclass
 // Debug test class
 class core_ibex_debug_test extends core_ibex_base_test;
 
-  bit[ibex_mem_intf_agent_pkg::DATA_WIDTH-1:0] irq_data;
-  bit[ibex_mem_intf_agent_pkg::DATA_WIDTH-1:0] debug_data;
+  bit[ibex_mem_intf_agent_pkg::DATA_WIDTH-1:0] core_start_data;
 
   `uvm_component_utils(core_ibex_debug_test)
   `uvm_component_new
@@ -51,13 +50,22 @@ class core_ibex_debug_test extends core_ibex_base_test;
     dut_vif.fetch_enable = 1'b1;
     fork
       vseq.start(env.vseqr);
-      if (cfg.enable_irq_seq) begin
-        wait_for_mem_txn(cfg.signature_addr, cfg.irq_start_req, irq_data);
-        vseq.start_irq_seq();
-      end
-      if (cfg.enable_debug_seq) begin
-        wait_for_mem_txn(cfg.signature_addr, cfg.debug_start_req, debug_data);
-        vseq.start_debug_seq();
+      begin
+        if (cfg.require_signature_addr) begin
+          wait_for_mem_txn(cfg.signature_addr, cfg.core_start_req, core_start_data);
+        end else begin
+          // If no signature_addr functionality is desired, then the test will
+          // simply wait for an adequate number of cycles
+          clk_vif.wait_clks(stimulus_delay);
+        end
+        fork
+          if (cfg.enable_irq_seq) begin
+            vseq.start_irq_seq();
+          end
+          if (cfg.enable_debug_seq) begin
+            vseq.start_debug_seq();
+          end
+        join_none
       end
     join_none
     wait_for_test_done();
