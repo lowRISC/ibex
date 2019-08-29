70b53068dbe71d374f8048e1be48529bab22a0e6
Tom Roberts <tomroberts@lowrisc.org>
2019-08-29 16:16:30 +0100

    [DV] Add registers testbench
    - Sample C++ unit testbench for system registers module
    - Only tests a few PMP registers at the moment

--- dv/cs_registers/tb/tb_cs_registers.cc
+++ dv/cs_registers/tb/tb_cs_registers.cc
@@ -2,27 +2,29 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-#include <signal.h>
-
-#include <iostream>
-
+#include "Vtb_cs_registers.h"
 #include "verilated_toplevel.h"
 #include "verilator_sim_ctrl.h"
 
-ibex_riscv_compliance *top;
+tb_cs_registers *top;
 VerilatorSimCtrl *simctrl;
 
+// dummy definition since this DPI call doesn't exist
+// TODO : remove this - see Ibex #317
+extern "C" {
+void simutil_verilator_memload(const char *file) {}
+}
+
 int main(int argc, char **argv) {
-  int retcode;
-  top = new ibex_riscv_compliance;
-  simctrl = new VerilatorSimCtrl(top, top->IO_CLK, top->IO_RST_N,
+  int retcode = 0;
+  // init top verilog instance
+  top = new tb_cs_registers;
+  // Create SimCtrl instance
+  simctrl = new VerilatorSimCtrl(top, top->clk_i, top->in_rst_ni,
                                  VerilatorSimCtrlFlags::ResetPolarityNegative);
 
-  // Setup simctrl
+  // Setup the simulation
   retcode = simctrl->SetupSimulation(argc, argv);
-
-  // Initialize RAM
-  simctrl->InitRam("TOP.ibex_riscv_compliance.u_ram");
 
   // Run the simulation
   simctrl->RunSimulation();
