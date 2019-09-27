05738e280db58f0a940f4deea443954a02f1602b
Udi <udij@google.com>
2019-09-27 14:27:09 -0700

    Update some test descriptions to avoid timeouts, update debug test

--- dv/uvm/tests/core_ibex_test_lib.sv
+++ dv/uvm/tests/core_ibex_test_lib.sv
@@ -346,6 +346,7 @@ class core_ibex_debug_wfi_test extends core_ibex_directed_test;
     // is detected, and before any stimulus is sent to the core
     forever begin
       wait (dut_vif.wfi === 1'b1);
+      wait (dut_vif.core_sleep === 1'b1);
       clk_vif.wait_clks($urandom_range(100));
       vseq.start_debug_single_seq();
       // After assserting this signal, core should wake up and jump into debug mode from WFI state
