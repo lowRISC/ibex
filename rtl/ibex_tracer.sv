3903fcb3d006878d5b5bdd8657ab5d16aed35458
Philipp Wagner <phw@lowrisc.org>
2019-10-02 18:32:40 +0100

    Tracer: Fix default file name
    We document the default file name to be trace_core_00000000.log, but
    actually used trace_core__00000000.log. Fix that.

--- rtl/ibex_tracer.sv
+++ rtl/ibex_tracer.sv
@@ -92,7 +92,7 @@ module ibex_tracer (
     string rvfi_insn_str;
 
     if (file_handle == 32'h0) begin
-      string file_name_base = "trace_core_";
+      string file_name_base = "trace_core";
       $value$plusargs("ibex_tracer_file_base=%s", file_name_base);
       $sformat(file_name, "%s_%h.log", file_name_base, hart_id_i);
 
