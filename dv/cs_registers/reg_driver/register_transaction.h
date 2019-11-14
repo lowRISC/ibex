d5f5a7550de16b6eed0137be2da106134cdb18d4
Tom Roberts <tomroberts@lowrisc.org>
2019-11-14 17:04:24 +0000

    [csr tb] Declare registers as a macro list
    All CSR addresses supported by the TB are now delared as an X
    macro list. This list is then used to autogenerate the enum
    type, string print helper and an array of all addresses.
    
    The array of addresses can now be used by the randomize generator
    to turn an arbitrary integer index into an address. This removes
    the restriction on only generating contiguous address ranges noted
    in #443.
    
    Add missing signal csr_restore_dtret_i
    
    Signed-off-by: Tom Roberts <tomroberts@lowrisc.org>

--- dv/cs_registers/reg_driver/register_transaction.h
+++ dv/cs_registers/reg_driver/register_transaction.h
@@ -9,36 +9,24 @@
 #include <random>
 #include <string>
 
+// Enumerate the supported register types by macro expansion
+enum CSRegisterAddr : int {
+#define CSR(reg, addr) kCSR##reg = addr,
+#include "csr_listing.def"
+};
+
+// Create an indexable array of all CSR addresses
+static const uint16_t CSRAddresses[] = {
+#define CSR(reg, addr) addr,
+#include "csr_listing.def"
+};
+
 // Enumerate the four register operation types
 enum CSRegisterOperation : int {
   kCSRRead = 0,
   kCSRWrite = 1,
   kCSRSet = 2,
   kCSRClear = 3
-};
-
-// Enumerate the supported register types
-enum CSRegisterAddr : int {
-  kCSRPMPCfg0 = 0x3A0,
-  kCSRPMPCfg1 = 0x3A1,
-  kCSRPMPCfg2 = 0x3A2,
-  kCSRPMPCfg3 = 0x3A3,
-  kCSRPMPAddr0 = 0x3B0,
-  kCSRPMPAddr1 = 0x3B1,
-  kCSRPMPAddr2 = 0x3B2,
-  kCSRPMPAddr3 = 0x3B3,
-  kCSRPMPAddr4 = 0x3B4,
-  kCSRPMPAddr5 = 0x3B5,
-  kCSRPMPAddr6 = 0x3B6,
-  kCSRPMPAddr7 = 0x3B7,
-  kCSRPMPAddr8 = 0x3B8,
-  kCSRPMPAddr9 = 0x3B9,
-  kCSRPMPAddr10 = 0x3BA,
-  kCSRPMPAddr11 = 0x3BB,
-  kCSRPMPAddr12 = 0x3BC,
-  kCSRPMPAddr13 = 0x3BD,
-  kCSRPMPAddr14 = 0x3BE,
-  kCSRPMPAddr15 = 0x3BF
 };
 
 struct RegisterTransaction {
