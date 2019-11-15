40d636898329354758cc54755b0142738955d344
Pirmin Vogel <vogelpi@lowrisc.org>
2019-11-15 12:37:48 +0100

    [dv] Remove clock gating primitive in `dv/uvm/tb`
    This commit removes a copy of the dummy clock gating primitive from
    tree previously used for UVM-based DV. There is another, identical copy
    of the same primitive in `shared/rtl` that can be used instead.
    
    This resolves lowRISC/ibex#213.
    
    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>

--- shared/rtl/prim_clock_gating.sv
+++ shared/rtl/prim_clock_gating.sv
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 //
-// Dummy clock gating module
+// Dummy clock gating module compatible with latch-based register file
 
 module prim_clock_gating (
     input        clk_i,
