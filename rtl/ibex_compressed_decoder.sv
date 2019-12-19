e9a6295e57e5ef83ba6fe4d1324a2abbb7032428
Pirmin Vogel <vogelpi@lowrisc.org>
2019-12-19 14:48:26 +0100

    [rtl] By default, forward incoming instruction in C decoder
    This commit modifies the compressed decoder to forward the incoming
    instruction to the output. It is marked as legal, unless:
    1) the decoder cannot determine if the instruction is compressed (e.g.
       because of unknown selector bits), or
    2) the instruction is compressed but
        a) it cannot be successfully decompressed (e.g. because of unknown
           selector bits), or
        b) it is indeed illegal.
    
    In the case of 2b) the compressed decoder may output an illegal
    decompressed instruction instead of the incoming instruction.
    
    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>

--- rtl/ibex_compressed_decoder.sv
+++ rtl/ibex_compressed_decoder.sv
@@ -25,10 +25,11 @@ module ibex_compressed_decoder (
   ////////////////////////
 
   always_comb begin
+    // By default, forward incoming instruction, mark it as legal.
+    instr_o         = instr_i;
     illegal_instr_o = 1'b0;
-    // Default instruction is NOP (ADDI x0, x0, 0)
-    instr_o         = {12'd0, 5'd0, 3'd0, 5'd0, OPCODE_OP_IMM};
 
+    // Check if incoming instruction is compressed.
     unique case (instr_i[1:0])
       // C0
       2'b00: begin
@@ -51,6 +52,14 @@ module ibex_compressed_decoder (
             instr_o = {5'b0, instr_i[5], instr_i[12], 2'b01, instr_i[4:2],
                        2'b01, instr_i[9:7], 3'b010, instr_i[11:10], instr_i[6],
                        2'b00, {OPCODE_STORE}};
+          end
+
+          3'b001,
+          3'b011,
+          3'b100,
+          3'b101,
+          3'b111: begin
+            illegal_instr_o = 1'b1;
           end
 
           default: begin
@@ -249,9 +258,11 @@ module ibex_compressed_decoder (
         endcase
       end
 
+      // Incoming instruction is not compressed.
+      2'b11:;
+
       default: begin
-        // 32 bit (or more) instruction
-        instr_o = instr_i;
+        illegal_instr_o = 1'b1;
       end
     endcase
   end
@@ -263,6 +274,8 @@ module ibex_compressed_decoder (
   ////////////////
 
   // Selectors must be known/valid.
+  `ASSERT_KNOWN(IbexInstrLSBsKnown, instr_i[1:0], clk_i, !rst_ni)
+  `ASSERT(IbexC0Known1, (instr_i[1:0] == 2'b00) |-> !$isunknown(instr_i[15:13]), clk_i, !rst_ni)
   `ASSERT(IbexC1Known1, (instr_i[1:0] == 2'b01) |-> !$isunknown(instr_i[15:13]), clk_i, !rst_ni)
   `ASSERT(IbexC1Known2,
       ((instr_i[1:0] == 2'b01) && (instr_i[15:13] == 3'b100)) |->
