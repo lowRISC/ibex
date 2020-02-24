f45b3eca99091ecd898ed263abccc9aade61daa5
Tobias Wölfel <tobias.woelfel@mailbox.org>
2020-02-24 12:10:16 +0100

    [rtl] Set RVFI program counter
    The next program counter is not always the program counter of the
    fetched instruction. When updating the counter, the actual next
    instruction is given by the branch target.

--- rtl/ibex_core.sv
+++ rtl/ibex_core.sv
@@ -1101,7 +1101,7 @@ module ibex_core #(
             rvfi_stage_rs2_addr[i]  <= rvfi_rs2_addr_d;
             rvfi_stage_rs3_addr[i]  <= rvfi_rs3_addr_d;
             rvfi_stage_pc_rdata[i]  <= pc_id;
-            rvfi_stage_pc_wdata[i]  <= pc_if;
+            rvfi_stage_pc_wdata[i]  <= pc_set ? branch_target_ex : pc_if;
             rvfi_stage_mem_rmask[i] <= rvfi_mem_mask_int;
             rvfi_stage_mem_wmask[i] <= data_we_o ? rvfi_mem_mask_int : 4'b0000;
             rvfi_stage_rs1_rdata[i] <= rvfi_rs1_data_d;
