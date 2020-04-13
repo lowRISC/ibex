d67847e6b8f88962b45ce5dd175cca9f980bdc68
Rupert Swarbrick <rswarbrick@lowrisc.org>
2020-04-13 17:07:55 +0100

    Add heartbeat tracking to ICache testbench
    This pretty much follows various UVM tutorials, except that we use the
    sequencers (ibex_icache_core_sequencer, ibex_icache_mem_sequencer) to
    poke the heartbeat objection object if there is one.
    
    Doing this means that we can dispense with the explicit test timeout
    in the hjson file: the test will fail if there is no activity on
    either interface for 2000 cycles (this number is chosen because the
    longest "wait a bit" time is 1200 cycles in the core driver).

--- dv/uvm/icache/dv/env/ibex_icache_env.sv
+++ dv/uvm/icache/dv/env/ibex_icache_env.sv
@@ -13,6 +13,11 @@ class ibex_icache_env extends dv_base_env #(
   ibex_icache_core_agent core_agent;
   ibex_icache_mem_agent  mem_agent;
 
+  // Heartbeat tracking
+  uvm_callbacks_objection           hb_objection;
+  uvm_heartbeat                     heartbeat;
+  uvm_event                         hb_event;
+
   `uvm_component_new
 
   function void build_phase(uvm_phase phase);
@@ -23,6 +28,10 @@ class ibex_icache_env extends dv_base_env #(
 
     mem_agent = ibex_icache_mem_agent::type_id::create("mem_agent", this);
     uvm_config_db#(ibex_icache_mem_agent_cfg)::set(this, "mem_agent*", "cfg", cfg.mem_agent_cfg);
+
+    hb_objection = new("hb_objection");
+    heartbeat    = new("heartbeat", this, hb_objection);
+    hb_event     = new("hb_event");
   endfunction
 
   function void connect_phase(uvm_phase phase);
@@ -37,6 +46,28 @@ class ibex_icache_env extends dv_base_env #(
     if (cfg.is_active && cfg.mem_agent_cfg.is_active) begin
       virtual_sequencer.mem_sequencer_h = mem_agent.sequencer;
     end
+
+    // Register the heartbeat objection with both sequencers (so they know how to reset it)
+    core_agent.sequencer.register_hb(hb_objection);
+    mem_agent.sequencer.register_hb(hb_objection);
+
+    // Add each sequencer to the heartbeat object. This means that the heartbeat object expects one
+    // or more of them to set the objection each time around.
+    void'(heartbeat.set_mode(UVM_ANY_ACTIVE));
+    heartbeat.add(core_agent.sequencer);
+    heartbeat.add(mem_agent.sequencer);
   endfunction
+
+  virtual task run_phase(uvm_phase phase);
+    super.run_phase(phase);
+    // Start the heartbeat monitor. This will check (and then clear) objections each time hb_event
+    // is triggered.
+    heartbeat.start(hb_event);
+    forever begin
+      // Every 2000 clocks, check the heartbeat monitor
+      cfg.core_agent_cfg.vif.wait_clks(2000);
+      hb_event.trigger();
+    end
+  endtask
 
 endclass
--- dv/uvm/icache/dv/ibex_icache_core_agent/ibex_icache_core_agent_pkg.sv
+++ dv/uvm/icache/dv/ibex_icache_core_agent/ibex_icache_core_agent_pkg.sv
@@ -17,23 +17,11 @@ package ibex_icache_core_agent_pkg;
   `include "uvm_macros.svh"
   `include "dv_macros.svh"
 
-  // parameters
-
-  // local types
-  // forward declare classes to allow typedefs below
-  typedef class ibex_icache_core_item;
-  typedef class ibex_icache_core_agent_cfg;
-
-  // reuse dv_base_seqeuncer as is with the right parameter set
-  typedef dv_base_sequencer #(.ITEM_T(ibex_icache_core_item),
-                              .CFG_T (ibex_icache_core_agent_cfg)) ibex_icache_core_sequencer;
-
-  // functions
-
   // package sources
   `include "ibex_icache_core_item.sv"
   `include "ibex_icache_core_agent_cfg.sv"
   `include "ibex_icache_core_agent_cov.sv"
+  `include "ibex_icache_core_sequencer.sv"
   `include "ibex_icache_core_driver.sv"
   `include "ibex_icache_core_monitor.sv"
   `include "ibex_icache_core_agent.sv"
