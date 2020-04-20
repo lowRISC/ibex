c83aac5501230613cff6f70bb83591595afb0c08
Jacek M. Wuwer <jacekmw8@gmail.com>
2020-04-20 13:25:21 +0200

    complete ibex sources

--- rtl/ibex_core.f
+++ rtl/ibex_core.f
@@ -2,7 +2,7 @@ ibex_pkg.sv
 ibex_alu.sv
 ibex_compressed_decoder.sv
 ibex_controller.sv
-ibex_counter.sv
+ibex_counters.sv
 ibex_cs_registers.sv
 ibex_decoder.sv
 ibex_ex_block.sv
@@ -14,4 +14,5 @@ ibex_multdiv_fast.sv
 ibex_prefetch_buffer.sv
 ibex_fetch_fifo.sv
 ibex_register_file_ff.sv
+ibex_wb_stage.sv
 ibex_core.sv
