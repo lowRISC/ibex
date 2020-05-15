ab3ff7d57e7416bfab2a0e0517d53cee76b26e48
Rupert Swarbrick <rswarbrick@lowrisc.org>
2020-05-15 15:00:04 +0100

    Use control knobs rather than subclassing in ICache core sequences
    This is a bit less verbose than what we had: we can just set the
    control knobs in the virtual sequence rather than subclassing
    everything.

--- dv/uvm/icache/dv/env/seq_lib/ibex_icache_passthru_vseq.sv
+++ dv/uvm/icache/dv/env/seq_lib/ibex_icache_passthru_vseq.sv
@@ -7,9 +7,8 @@ class ibex_icache_passthru_vseq extends ibex_icache_base_vseq;
   `uvm_object_utils(ibex_icache_passthru_vseq)
   `uvm_object_new
 
-  // A passthru sequence for the core agent and a basic slave sequence for the memory agent
-  ibex_icache_core_passthru_seq core_seq;
-  ibex_icache_mem_resp_seq      mem_seq;
+  ibex_icache_core_base_seq core_seq;
+  ibex_icache_mem_resp_seq  mem_seq;
 
   task body();
     // Start the core and memory sequences. We use fork/join_any so that we don't wait for the
@@ -17,11 +16,17 @@ class ibex_icache_passthru_vseq extends ibex_icache_base_vseq;
     fork
       begin
         `uvm_create_on(core_seq, p_sequencer.core_sequencer_h)
+
+        // Constrain branch targets and leave the cache disabled.
+        core_seq.constrain_branches = 1'b1;
+        core_seq.force_disable      = 1'b1;
+
         `DV_CHECK_RANDOMIZE_FATAL(core_seq)
         core_seq.start(p_sequencer.core_sequencer_h);
       end
       begin
         `uvm_create_on(mem_seq, p_sequencer.mem_sequencer_h)
+
         // Increase the frequency of seed updates
         mem_seq.gap_between_seeds = 49;
 
