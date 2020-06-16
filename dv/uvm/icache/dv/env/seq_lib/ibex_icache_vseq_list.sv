5776c0b692a72cafa2b7e0c7b8dc64c824133f4e
Rupert Swarbrick <rswarbrick@lowrisc.org>
2020-06-16 11:58:59 +0100

    Remove ibex_icache_sanity_vseq
    This derived from ibex_icache_base_vseq but didn't do any other
    customisation. Let's just use the base vseq for the test.

--- dv/uvm/icache/dv/env/seq_lib/ibex_icache_vseq_list.sv
+++ dv/uvm/icache/dv/env/seq_lib/ibex_icache_vseq_list.sv
@@ -3,7 +3,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 `include "ibex_icache_base_vseq.sv"
-`include "ibex_icache_sanity_vseq.sv"
 `include "ibex_icache_passthru_vseq.sv"
 `include "ibex_icache_caching_vseq.sv"
 `include "ibex_icache_invalidation_vseq.sv"
