16e3c82426bcf838462f6c9ea24ec93a326f69bd
Bert Pieters <bert.pieters@gmail.com>
2020-06-19 10:09:44 +0200

    [rtl] disable clock between reset and fetch_enable_i
    Fixes lowRISC#957
    
    Signed-off-by: Bert Pieters <bert.pieters@gmail.com>

--- rtl/ibex_id_stage.sv
+++ rtl/ibex_id_stage.sv
@@ -28,7 +28,6 @@ module ibex_id_stage #(
     input  logic                      clk_i,
     input  logic                      rst_ni,
 
-    input  logic                      fetch_enable_i,
     output logic                      ctrl_busy_o,
     output logic                      illegal_insn_o,
 
@@ -526,7 +525,6 @@ module ibex_id_stage #(
       .clk_i                          ( clk_i                   ),
       .rst_ni                         ( rst_ni                  ),
 
-      .fetch_enable_i                 ( fetch_enable_i          ),
       .ctrl_busy_o                    ( ctrl_busy_o             ),
 
       // decoder related signals
