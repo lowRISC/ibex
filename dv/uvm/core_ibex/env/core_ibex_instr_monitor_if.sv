7b20f56aa5ca636fb13c7d8b48ec31179878069a
Udi <udij@google.com>
2020-07-08 13:25:00 -0700

    [dv/ibex] Enhance riscv_debug_single_step test
    As pointed out by @tomroberts-lowrisc in #983, the current
    implementation of riscv_debug_single_step_test cannot handle
    single-stepping over instructions that change the PC.
    This PR aims to introduce this functionality, utilizing the
    new instr_monitor_if.
    
    Now, if the core single-steps onto a branch/jump instruction, the
    testbench will log the new target PC and compare it against the actual
    target address stored in `dpc`.
    "Normal" instructions are checked as usual by incrementing the
    instruction's PC by either 2 or 4 (depending whether it is compressed)
    and comparing that against `dpc`.

--- dv/uvm/core_ibex/env/core_ibex_instr_monitor_if.sv
+++ dv/uvm/core_ibex/env/core_ibex_instr_monitor_if.sv
@@ -6,7 +6,11 @@
 //
 // TODO: does not support dummy instruction insertion right now,
 //       might have to revisit and update.
-interface core_ibex_instr_monitor_if #(parameter DATA_WIDTH = 32);
+interface core_ibex_instr_monitor_if #(
+  parameter DATA_WIDTH = 32
+) (
+  input clk
+);
 
   // ID stage
   logic                   valid_id;
@@ -14,5 +18,19 @@ interface core_ibex_instr_monitor_if #(parameter DATA_WIDTH = 32);
   logic                   is_compressed_id;
   logic [15:0]            instr_compressed_id;
   logic [DATA_WIDTH-1:0]  instr_id;
+  logic [DATA_WIDTH-1:0]  pc_id;
+  logic                   branch_taken_id;
+  logic [DATA_WIDTH-1:0]  branch_target_id;
+
+  clocking instr_cb @(posedge clk);
+    input valid_id;
+    input err_id;
+    input is_compressed_id;
+    input instr_compressed_id;
+    input instr_id;
+    input pc_id;
+    input branch_taken_id;
+    input branch_target_id;
+  endclocking
 
 endinterface
