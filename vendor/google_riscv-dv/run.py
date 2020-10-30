7aeb2072aac812dfa394115ddcb9075c50695d42
Udi Jonnalagadda <udij@google.com>
2020-10-30 15:59:26 -0700

    Update google_riscv-dv to google/riscv-dv@3467c37
    Update code from upstream repository https://github.com/google/riscv-
    dv to revision 3467c3777cb428b2e30b30b7f895a8fd73873d4f
    
    * add VCS compile option for unicode (Udi Jonnalagadda)
    * Add missing license header (aneels3)
    * [Docs] Fix broken links and typos (db434)
    * Add support for RV32D (ishita71)
    * Fix google/riscv-dv#733 (aneels3)
    * Fix Spike Issue (aneels3)
    * add riscv_reg and riscv_privil_reg (pvipsyash)
    * fix target issue for foating point (pvipsyash)
    * add rv32fc target (pvipsyash)
    * add riscv_floating_point_instr (pvipsyash)
    * Add defines for floating point instructions (ShraddhaDevaiya)
    
    Signed-off-by: Udi Jonnalagadda <udij@google.com>

--- vendor/google_riscv-dv/run.py
+++ vendor/google_riscv-dv/run.py
@@ -756,7 +756,7 @@ def parse_args(cwd):
 
     parser.add_argument("--target", type=str, default="rv32imc",
                         help="Run the generator with pre-defined targets: \
-                            rv32imc, rv32i, rv64imc, rv64gc")
+                            rv32imc, rv32i, rv32imfdc, rv64imc, rv64gc")
     parser.add_argument("-o", "--output", type=str,
                         help="Output directory name", dest="o")
     parser.add_argument("-tl", "--testlist", type=str, default="",
@@ -918,6 +918,9 @@ def load_config(args, cwd):
         if args.target == "rv32imc":
             args.mabi = "ilp32"
             args.isa = "rv32imc"
+        elif args.target == "rv32imfdc":
+            args.mabi = "ilp32"
+            args.isa = "rv32imfdc"
         elif args.target == "rv32imc_sv32":
             args.mabi = "ilp32"
             args.isa = "rv32imc"
