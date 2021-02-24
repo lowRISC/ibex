6ebc6bcb9f4574ab2adff9ec1c27f052ee620b69
Rupert Swarbrick <rswarbrick@lowrisc.org>
2021-02-24 15:07:06 +0000

    [simple_system] Fix type for mhpmcounter_get
    It's probably clearer if this 64-bit counter is treated as a uint64_t,
    not an int64_t (the code using it downstream expects non-negative
    values).

--- examples/simple_system/rtl/ibex_simple_system.sv
+++ examples/simple_system/rtl/ibex_simple_system.sv
@@ -279,7 +279,7 @@ module ibex_simple_system (
 
   export "DPI-C" function mhpmcounter_get;
 
-  function automatic longint mhpmcounter_get(int index);
+  function automatic longint unsigned mhpmcounter_get(int index);
     return u_core.u_ibex_core.cs_registers_i.mhpmcounter[index];
   endfunction
 
