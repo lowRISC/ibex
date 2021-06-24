8df7623e826b9d53d8444989b3af9bd0df4e1169
Greg Chadwick <gac@lowrisc.org>
2021-06-24 13:24:48 +0100

    [dv] Improve results reporting
    This commits adds a yaml based intermediate format for test results.
    compare.py serialises a TestRunResult (a named tuple type) into this
    format for each test run it checks. collect_results.py reads them all
    back in to produce reports.
    
    Three reports are output:
    - regr.log - plain text report much like the one previously produced
    - regr_junit.xml, regr_junit_merged.xml - JUnit report format, the
      _merged version batches together multiple tests to appear to be a
      single test case under a test suite. This gives better results with
      Azure's JUnit reporting.

--- dv/uvm/core_ibex/compare.py
+++ dv/uvm/core_ibex/compare.py
@@ -11,9 +11,10 @@ import argparse
 import os
 import re
 import sys
-from typing import Dict, Optional, TextIO, Tuple
+from typing import Dict, Optional, TextIO, Tuple, Union
 
 from test_entry import TestEntry, get_test_entry
+from test_run_result import TestRunResult
 
 _CORE_IBEX = os.path.normpath(os.path.join(os.path.dirname(__file__)))
 _IBEX_ROOT = os.path.normpath(os.path.join(_CORE_IBEX, '../../..'))
@@ -58,7 +59,8 @@ def compare_test_run(test: TestEntry,
                      seed: int,
                      rtl_log_dir: str,
                      iss: str,
-                     iss_log_dir: str) -> _CompareResult:
+                     iss_log_dir: str,
+                     instr_gen_bin_dir: str) -> TestRunResult:
     '''Compare results for a single run of a single test
 
     Here, test is a dictionary describing the test (read from the testlist YAML
@@ -76,30 +78,50 @@ def compare_test_run(test: TestEntry,
     test_name = test['test']
     assert isinstance(test_name, str)
     uvm_log = os.path.join(rtl_log_dir, 'sim.log')
+    elf = os.path.join(instr_gen_bin_dir, '{}_{}.o'.format(test_name, idx))
+
+    rtl_trace = os.path.join(rtl_log_dir, 'trace_core_00000000.log')
 
     kv_data = {
-        'test name': test_name,
-        'iteration': str(idx),
-        'seed': str(seed),
-        'UVM log': uvm_log
+        'name': test_name,
+        'idx': idx,
+        'seed': seed,
+        'binary': elf,
+        'uvm_log': uvm_log,
+        'rtl_trace': rtl_trace,
+        'rtl_trace_csv': None,
+        'iss_trace': None,
+        'iss_trace_csv': None,
+        'comparison_log': None,
+        'passed': False,
+        'failure_message': None
     }
 
     # Have a look at the UVM log. Report a failure if an issue is seen in the
     # log.
-    uvm_pass, uvm_log_lines = check_ibex_uvm_log(uvm_log)
+    try:
+        uvm_pass, uvm_log_lines = check_ibex_uvm_log(uvm_log)
+    except IOError as e:
+        kv_data['failure_message'] = str(e)
+        kv_data['failure_message'] += \
+            '\n[FAILED] Could not open simulation log'
+        return TestRunResult(**kv_data)
+
     if not uvm_pass:
-        return (False, 'simulation error', kv_data)
+        kv_data['failure_message'] = '\n'.join(uvm_log_lines)
+        kv_data['failure_message'] += '\n[FAILED]: sim error seen'
+        return TestRunResult(**kv_data)
 
-    rtl_log = os.path.join(rtl_log_dir, 'trace_core_00000000.log')
-    rtl_csv = os.path.join(rtl_log_dir, 'trace_core_00000000.csv')
+    rtl_trace_csv = os.path.join(rtl_log_dir, 'trace_core_00000000.csv')
 
-    kv_data['rtl log'] = rtl_log
-    kv_data['rtl csv'] = rtl_csv
+    kv_data['rtl_trace_csv'] = rtl_trace_csv
     try:
         # Convert the RTL log file to a trace CSV.
-        process_ibex_sim_log(rtl_log, rtl_csv, 1)
+        process_ibex_sim_log(rtl_trace, rtl_trace_csv, 1)
     except (OSError, RuntimeError) as e:
-        return (False, f'RTL log processing failed ({e})', kv_data)
+        kv_data['failure_message'] = \
+            '[FAILED]: Log processing failed: {}'.format(e)
+        return TestRunResult(**kv_data)
 
     no_post_compare = test.get('no_post_compare', False)
     assert isinstance(no_post_compare, bool)
@@ -107,14 +129,15 @@ def compare_test_run(test: TestEntry,
     # no_post_compare skips the final ISS v RTL log check, so if we've reached
     # here we're done when no_post_compare is set.
     if no_post_compare:
-        return (True, None, kv_data)
+        kv_data['passed'] = True
+        return TestRunResult(**kv_data)
 
     # There were no UVM errors. Process the log file from the ISS.
     iss_log = os.path.join(iss_log_dir, '{}.{}.log'.format(test_name, idx))
     iss_csv = os.path.join(iss_log_dir, '{}.{}.csv'.format(test_name, idx))
 
-    kv_data['ISS log'] = iss_log
-    kv_data['ISS csv'] = iss_csv
+    kv_data['iss_trace'] = iss_log
+    kv_data['iss_trace_csv'] = iss_csv
     try:
         if iss == "spike":
             process_spike_sim_log(iss_log, iss_csv)
@@ -122,10 +145,12 @@ def compare_test_run(test: TestEntry,
             assert iss == 'ovpsim'  # (should be checked by argparse)
             process_ovpsim_sim_log(iss_log, iss_csv)
     except (OSError, RuntimeError) as e:
-        return (False, f'ISS log processing failed ({e})', kv_data)
+        kv_data['failure_message'] = \
+            '[FAILED]: Log processing failed: {}'.format(e)
+        return TestRunResult(**kv_data)
 
     compare_log = os.path.join(rtl_log_dir, 'compare.log')
-    kv_data['comparison log'] = compare_log
+    kv_data['comparison_log'] = compare_log
 
     # Delete any existing file at compare_log (the compare_trace_csv function
     # would append to it, which is rather confusing).
@@ -135,36 +160,59 @@ def compare_test_run(test: TestEntry,
         pass
 
     compare_result = \
-        compare_trace_csv(rtl_csv, iss_csv, "ibex", iss, compare_log,
+        compare_trace_csv(rtl_trace_csv, iss_csv, "ibex", iss, compare_log,
                           **test.get('compare_opts', {}))
+
+    try:
+        compare_log_file = open(compare_log)
+        compare_log_contents = compare_log_file.read()
+        compare_log_file.close()
+    except IOError as e:
+        kv_data['failure_message'] = \
+            '[FAILED]: Could not read compare log: {}'.format(e)
+        return TestRunResult(**kv_data)
 
     # Rather oddly, compare_result is a string. The comparison passed if it
     # starts with '[PASSED]' and failed otherwise.
     compare_passed = compare_result.startswith('[PASSED]: ')
     if not compare_passed:
         assert compare_result.startswith('[FAILED]: ')
-        # compare_result[10:] will look like "123 matched, 321 mismatch",
-        # meaning that 123 instructions matched and 321 instructions didn't.
-        kv_data['compared instructions'] = compare_result[10:]
-        return (False, 'mismatch between ISS and RTL', kv_data)
+        kv_data['failure_message'] = ('RTL / ISS trace comparison failed\n' +
+                                      compare_log_contents)
+        return TestRunResult(**kv_data)
 
-    # compare_result[10:] will look like "123 matched", meaning that 123
-    # instructions matched.
-    kv_data['compared instructions'] = compare_result[10:]
-    return (True, None, kv_data)
+    kv_data['passed'] = True
+    return TestRunResult(**kv_data)
 
 
-def on_result(result: _CompareResult, output: TextIO) -> None:
-    passed, err_msg, kv_data = result
+# If any of these characters are present in a string output it in multi-line
+# mode. This will either be because the string contains newlines or other
+# characters that would otherwise need escaping
+_YAML_MULTILINE_CHARS = ['[', ']', ':', "'", '"', '\n']
 
-    if passed:
-        assert err_msg is None
-        output.write('PASS\n\n')
-    else:
-        assert err_msg is not None
-        output.write('FAIL\n\n')
-        output.write(f'Test failed: {err_msg}\n')
-        output.write('---\n\n')
+
+def yaml_format(val: Union[int, str, bool]) -> str:
+    '''Format a value for yaml output.
+
+    For int, str and bool value can just be converted to str with special
+    handling for some string
+    '''
+
+    # If val is a multi-line string
+    if isinstance(val, str) and any(c in val for c in _YAML_MULTILINE_CHARS):
+        # Split into individual lines and output them after a suitable yaml
+        # multi-line string indicator ('|-') indenting each line.
+        lines = val.split('\n')
+        return '|-\n' + '\n'.join([f'  {line}' for line in lines])
+
+    if val is None:
+        return ''
+
+    return str(val)
+
+
+def on_result(result: TestRunResult, output: TextIO) -> None:
+    kv_data = result._asdict()
 
     klen = 1
     for k in kv_data:
@@ -172,11 +220,12 @@ def on_result(result: _CompareResult, output: TextIO) -> None:
 
     for k, v in kv_data.items():
         kpad = ' ' * (klen - len(k))
-        output.write(f'{k}:{kpad} | {v}\n')
+        output.write(f'{k}:{kpad} {yaml_format(v)}\n')
 
 
 def main() -> int:
     parser = argparse.ArgumentParser()
+    parser.add_argument('--instr-gen-bin-dir', required=True)
     parser.add_argument('--iss', required=True, choices=['spike', 'ovpsim'])
     parser.add_argument('--iss-log-dir', required=True)
     parser.add_argument('--start-seed', type=int, required=True)
@@ -200,7 +249,8 @@ def main() -> int:
     entry = get_test_entry(testname)
 
     result = compare_test_run(entry, iteration, seed,
-                              args.rtl_log_dir, args.iss, args.iss_log_dir)
+                              args.rtl_log_dir, args.iss, args.iss_log_dir,
+                              args.instr_gen_bin_dir)
     with open(args.output, 'w', encoding='UTF-8') as outfile:
         on_result(result, outfile)
 
