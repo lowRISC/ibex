6a10b25404fdd6b46c0dc39583893cb2607a04eb
Greg Chadwick <gac@lowrisc.org>
2021-09-10 15:32:11 +0100

    [examples/fpga] Use 64 kB memory by default
    The 256 kB is too large for the Arty-A7 with the A7-35T. Only use it
    for the FPGA power analysis configuration.

--- examples/fpga/artya7/rtl/top_artya7.sv
+++ examples/fpga/artya7/rtl/top_artya7.sv
@@ -8,10 +8,13 @@ module top_artya7 (
     output [3:0]        LED
 );
 
-  parameter int          MEM_SIZE     = 256 * 1024; // 256 kB
-  parameter logic [31:0] MEM_START    = 32'h00000000;
-  parameter logic [31:0] MEM_MASK     = MEM_SIZE-1;
-  parameter              SRAMInitFile = "";
+  parameter int          FPGAPowerAnalysis = 0;
+  // Choose 64kb memory for normal builds and 256kb for FPGAPowerAnalysis builds. The latter will
+  // not fit in the Arty A7-35 Board FPGA.
+  parameter int          MEM_SIZE          = FPGAPowerAnalysis == 0 ? 64 * 1024 : 256 * 1024;
+  parameter logic [31:0] MEM_START         = 32'h00000000;
+  parameter logic [31:0] MEM_MASK          = MEM_SIZE-1;
+  parameter              SRAMInitFile      = "";
 
   logic clk_sys, rst_sys_n;
 
