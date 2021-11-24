dee7228847dc418837208d3a10eb9bc31fad0d94
phthinh <hung3@e.ntu.edu.sg>
2021-11-24 11:19:40 +0000

    Zk: add the implementation for AES instructions

--- rtl/ibex_zk.sv
+++ rtl/ibex_zk.sv
@@ -21,7 +21,79 @@ module ibex_zk #(
 `define SRLI32(a,b) ((a >> b)              )
 `define SLLI32(a,b) ((a << b)              )
 
+// Multiply by 2 in GF(2^8) modulo 8'h1b
+function automatic logic [7:0] xtime2(logic [7:0] a);
+    logic [7:0] xtime2;
+    xtime2  = {a[6:0],1'b0} ^ (a[7] ? 8'h1b : 8'b0 );
+    return xtime2;
+endfunction
+
+// Paired down multiply by X in GF(2^8)
+function automatic logic [7:0] xtimeN(logic [7:0] a, logic [3:0] b);
+    logic [7:0] xtimeN;
+    xtimeN = (b[0] ?                      a   : 0) ^
+             (b[1] ? xtime2(              a)  : 0) ^
+             (b[2] ? xtime2(xtime2(       a)) : 0) ^
+             (b[3] ? xtime2(xtime2(xtime2(a))): 0) ;
+    return xtimeN;
+endfunction
+
   if (RV32Zk == RV32Zkn) begin : gen_zkn
+    logic bs0, bs1, bs2, bs3; //byte select in aes instructions
+    assign bs0 = (operator_i == ZKN_AES32DSB0) || (operator_i == ZKN_AES32DSMB0) ||
+                 (operator_i == ZKN_AES32ESB0) || (operator_i == ZKN_AES32ESMB0) ;
+    assign bs1 = (operator_i == ZKN_AES32DSB1) || (operator_i == ZKN_AES32DSMB1) ||
+                 (operator_i == ZKN_AES32ESB1) || (operator_i == ZKN_AES32ESMB1) ;
+    assign bs2 = (operator_i == ZKN_AES32DSB2) || (operator_i == ZKN_AES32DSMB2) ||
+                 (operator_i == ZKN_AES32ESB2) || (operator_i == ZKN_AES32ESMB2) ;
+    assign bs3 = (operator_i == ZKN_AES32DSB3) || (operator_i == ZKN_AES32DSMB3) ||
+                 (operator_i == ZKN_AES32ESB3) || (operator_i == ZKN_AES32ESMB3) ;
+
+    logic decs_sel, encs_sel, decsm_sel, encsm_sel; //operation select in aes instructions
+    assign decs_sel  = (operator_i == ZKN_AES32DSB0)  || (operator_i == ZKN_AES32DSB1)  ||
+                       (operator_i == ZKN_AES32DSB2)  || (operator_i == ZKN_AES32DSB3)  ;
+    assign encs_sel  = (operator_i == ZKN_AES32ESB0)  || (operator_i == ZKN_AES32ESB1)  ||
+                       (operator_i == ZKN_AES32ESB2)  || (operator_i == ZKN_AES32ESB3)  ;
+    assign decsm_sel = (operator_i == ZKN_AES32DSMB0) || (operator_i == ZKN_AES32DSMB1) ||
+                       (operator_i == ZKN_AES32DSMB2) || (operator_i == ZKN_AES32DSMB3) ;
+    assign encsm_sel = (operator_i == ZKN_AES32ESMB0) || (operator_i == ZKN_AES32ESMB1) ||
+                       (operator_i == ZKN_AES32ESMB2) || (operator_i == ZKN_AES32ESMB3) ;
+    logic  aes32_sel;
+    assign aes32_sel = decs_sel || encs_sel || encsm_sel || decsm_sel;
+
+    logic  [7:0] sel_byte;
+    assign       sel_byte = {8{bs0}} & operand_b_i[ 7: 0] |
+                            {8{bs1}} & operand_b_i[15: 8] |
+                            {8{bs2}} & operand_b_i[23:16] |
+                            {8{bs3}} & operand_b_i[31:24] ;
+
+    logic  dec, mix;
+    assign dec      = decs_sel  || decsm_sel  ;
+    assign mix      = encsm_sel || decsm_sel  ;
+
+    logic [7:0] sbox_out;
+    // SBOX instances
+    ibex_aes_sbox  i_aes_sbox(
+        .fw (~dec            ),
+        .in (sel_byte    ),
+        .fx (sbox_out)
+    );
+
+    logic [7:0] mix_b0, mix_b1, mix_b2, mix_b3;
+    assign mix_b3 =       xtimeN(sbox_out, (dec ? 11  : 3))            ;
+    assign mix_b2 = dec ? xtimeN(sbox_out, (           13)) : sbox_out ;
+    assign mix_b1 = dec ? xtimeN(sbox_out, (            9)) : sbox_out ;
+    assign mix_b0 =       xtimeN(sbox_out, (dec ? 14  : 2))            ;
+
+    logic [31:0] mixed, sbox_mix, rotated;
+    assign mixed    = {mix_b3, mix_b2, mix_b1, mix_b0};
+    assign sbox_mix = mix ? mixed : {24'b0, sbox_out};
+    assign rotated  = {32{bs0}} & {sbox_mix                        } |
+                      {32{bs1}} & {sbox_mix[23:0], sbox_mix[31:24] } |
+                      {32{bs2}} & {sbox_mix[15:0], sbox_mix[31:16] } |
+                      {32{bs3}} & {sbox_mix[ 7:0], sbox_mix[31: 8] } ;
+
+    // sha2 instructions
     logic  sha256_sum0_sel, sha256_sum1_sel, sha256_sig0_sel, sha256_sig1_sel;
     assign sha256_sum0_sel  = (operator_i == ZKN_SHA256SUM0);
     assign sha256_sum1_sel  = (operator_i == ZKN_SHA256SUM1);
@@ -64,8 +136,9 @@ module ibex_zk #(
     assign zk_val_o     = |{sha256_sum0_sel, sha256_sum1_sel, sha256_sig0_sel, sha256_sig1_sel,
                            sha512_sum0r_sel, sha512_sum1r_sel,
                            sha512_sig0l_sel, sha512_sig1l_sel,
-                           sha512_sig0h_sel, sha512_sig1h_sel  };
-    assign result_o     = {32{sha256_sig0_sel }} & sha256_sig0  |
+                           sha512_sig0h_sel, sha512_sig1h_sel, aes32_sel};
+    assign result_o     = {32{aes32_sel       }} & (rotated ^ operand_a_i) |
+                          {32{sha256_sig0_sel }} & sha256_sig0  |
                           {32{sha256_sig1_sel }} & sha256_sig1  |
                           {32{sha256_sum0_sel }} & sha256_sum0  |
                           {32{sha256_sum1_sel }} & sha256_sum1  |
