f1131576cbed5bc8b81a3ff5cf4e249443fec6ed
Rupert Swarbrick <rswarbrick@lowrisc.org>
2022-02-17 16:23:51 +0000

    Update ISA strings from Xbitmanip to XZb*
    This allows us to model stuff more closely. This depends on Spike
    version ibex-cosim-v0.2 (which is rebased onto a master branch commit
    supporting these more specific ISA strings).

--- examples/simple_system/ibex_simple_system.cc
+++ examples/simple_system/ibex_simple_system.cc
@@ -43,19 +43,20 @@ std::string SimpleSystem::GetIsaString() const {
   if (top.ibex_simple_system->RV32M)
     extensions += "m";
 
-  // See the equivalent get_isa_string() function in core_ibex_base_test.sv for
-  // an explanation of the different ISA strings
   switch (top.ibex_simple_system->RV32B) {
     case 0:  // RV32BNone
       break;
 
     case 1:  // RV32BBalanced
-      extensions += "_Zba_Zbb_Zbs_Xbitmanip";
+      extensions += "_Zba_Zbb_Zbs_XZbf_XZbt";
       break;
 
     case 2:  // RV32BOTEarlGrey
+      extensions += "_Zba_Zbb_Zbc_Zbs_XZbf_XZbp_XZbr_XZbt";
+      break;
+
     case 3:  // RV32BFull
-      extensions += "_Zba_Zbb_Zbc_Zbs_Xbitmanip";
+      extensions += "_Zba_Zbb_Zbc_Zbs_XZbe_XZbf_XZbp_XZbr_XZbt";
       break;
   }
 
