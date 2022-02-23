838c8d79f0fdd35e3fdb12778452c82ecbfcaa55
Prajwala Puttappa <prajwalaputtappa@lowrisc.org>
2022-02-23 16:09:57 +0000

    [ibex, dv] Added agent configuration for ibex_mem_intf_response_agent
    Defining agent configuration for any agent is a standard UVM flow and is
    a cleaner flow for defining delay between driving sequence items,
    passing virtual interface etc.
    
    Agent configuration has been added to the existing agent to make delay
    configuration more flexible in the future.
    
    Signed-off-by: Prajwala Puttappa <prajwalaputtappa@lowrisc.org>

--- dv/uvm/core_ibex/common/ibex_mem_intf_agent/ibex_mem_intf_response_agent.sv
+++ dv/uvm/core_ibex/common/ibex_mem_intf_agent/ibex_mem_intf_response_agent.sv
@@ -10,7 +10,8 @@ class ibex_mem_intf_response_agent extends uvm_agent;
 
   ibex_mem_intf_response_driver     driver;
   ibex_mem_intf_response_sequencer  sequencer;
-  ibex_mem_intf_monitor          monitor;
+  ibex_mem_intf_monitor             monitor;
+  ibex_mem_intf_response_agent_cfg  cfg;
 
   `uvm_component_utils(ibex_mem_intf_response_agent)
   `uvm_component_new
@@ -18,10 +19,13 @@ class ibex_mem_intf_response_agent extends uvm_agent;
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
     monitor = ibex_mem_intf_monitor::type_id::create("monitor", this);
+    cfg = ibex_mem_intf_response_agent_cfg::type_id::create("cfg", this);
     if(get_is_active() == UVM_ACTIVE) begin
       driver = ibex_mem_intf_response_driver::type_id::create("driver", this);
       sequencer = ibex_mem_intf_response_sequencer::type_id::create("sequencer", this);
     end
+    if(!uvm_config_db#(virtual ibex_mem_intf)::get(this, "", "vif", cfg.vif))
+      `uvm_fatal("NOVIF",{"virtual interface must be set for: ",get_full_name(),".vif"});
   endfunction : build_phase
 
   function void connect_phase(uvm_phase phase);
@@ -30,6 +34,7 @@ class ibex_mem_intf_response_agent extends uvm_agent;
       driver.seq_item_port.connect(sequencer.seq_item_export);
       monitor.addr_ph_port.connect(sequencer.addr_ph_port.analysis_export);
     end
+    driver.vif = cfg.vif;
   endfunction : connect_phase
 
   function void reset();
