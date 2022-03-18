7508644c6ebd19906ca3d8d640119b6a5484eb89
Greg Chadwick <gac@lowrisc.org>
2022-03-18 17:28:20 +0000

    [simple_system_cosim] Switch to latest cosim version

--- dv/verilator/simple_system_cosim/util/ibex_cosim_setup_check.sh
+++ dv/verilator/simple_system_cosim/util/ibex_cosim_setup_check.sh
@@ -4,10 +4,11 @@
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
 
-if [[ ! -v IBEX_COSIM_ISS_ROOT ]]; then
-  echo "IBEX_COSIM_ISS_ROOT must be set to the root directory of a suitable" \
-       "modified spike implementation, see" \
-       "dv/verilator/simple_system_cosim/README.md for more details"
-  exit 1;
-fi
+SPIKE_PCS="riscv-riscv riscv-disasm riscv-fdt"
 
+if !(pkg-config --exists $SPIKE_PCS)
+then
+  echo "Failed to find Spike pkg-config packages. Did you add lib/pkgconfig"\
+       "from your spike install to PKG_CONFIG_PATH?"
+  exit 1
+fi
