53df2cae9ad255781086ad92aa08c3ad2ae84116
Rupert Swarbrick <rswarbrick@lowrisc.org>
2022-04-19 17:41:39 +0100

    Split calls to compile-generated-tests.py
    This also involves a bit of file renaming, which makes the stuff
    downstream a bit easier.

--- dv/uvm/core_ibex/scripts/run-iss.py
+++ dv/uvm/core_ibex/scripts/run-iss.py
@@ -8,33 +8,18 @@ import argparse
 import os
 import sys
 
-from scripts_lib import read_test_dot_seed, run_one
+from scripts_lib import run_one
 
 
 def main() -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument('--verbose', action='store_true')
     parser.add_argument('--iss', required=True)
-    parser.add_argument('--test-dot-seed',
-                        type=read_test_dot_seed, required=True)
-    parser.add_argument('--gen-dir', required=True)
-    parser.add_argument('--run-dir', required=True)
+    parser.add_argument('--input', required=True)
+    parser.add_argument('--output', required=True)
     parser.add_argument('--isa', required=True)
 
-    parser.add_argument('--start-seed', type=int, required=True)
-
     args = parser.parse_args()
-
-    testname, seed = args.test_dot_seed
-
-    if args.start_seed < 0:
-        raise RuntimeError('Invalid start seed: {}'.format(args.start_seed))
-    testname, seed = args.test_dot_seed
-    if seed < args.start_seed:
-        raise RuntimeError('Start seed is greater than test seed '
-                           f'({args.start_seed} > {seed}).')
-
-    iteration = seed - args.start_seed
 
     # riscv-dv knows how to run an ISS simulation (see yaml/iss.yaml in the
     # vendored directory), but it has definite (and inconvenient!) opinions
@@ -48,21 +33,16 @@ def main() -> int:
     if args.iss != 'spike':
         raise RuntimeError(f'Unsupported ISS: {args.iss}')
 
-    object_name = f'{testname}_{iteration}.o'
-    log_name = os.path.join(args.run_dir, f'{testname}.{seed}.log')
-
     spike_dir = os.getenv('SPIKE_PATH')
     if spike_dir is not None:
         spike = os.path.join(spike_dir, 'spike')
     else:
         spike = 'spike'
 
-    cmd = [spike, '--log-commits',
-           '--isa', args.isa,
-           '-l', os.path.join(args.gen_dir, 'asm_test', object_name)]
+    cmd = [spike, '--log-commits', '--isa', args.isa, '-l', args.input]
     return run_one(args.verbose,
                    cmd,
-                   redirect_stdstreams=log_name)
+                   redirect_stdstreams=args.output)
 
 
 if __name__ == '__main__':
