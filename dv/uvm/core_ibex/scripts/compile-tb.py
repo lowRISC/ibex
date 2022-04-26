15cf40a1036f49cd5dbd55fa5d8814a83a3289af
Harry Callahan <hcallahan@lowrisc.org>
2022-04-26 14:50:26 +0100

    Construct compile-tb cmds to link against ISS for cosim
    This is a continuation of PR's #1613 and #1575 work.
    
    If cosim in enabled, we need to pass the appropriate flags to the eda tool for
    it to link against the precompiled ISS when building the testbench.
    This commit assembles the appropriate flags using pkg-config to query the
    SPIKE_ISS build, then uses the scripts_lib.subst_vars() method to populate the
    templated commands in the yaml.
    
    The eda tools have different requirements for consuming the flags, so massage
    them into the appropriate shape on the python side.

--- dv/uvm/core_ibex/scripts/compile-tb.py
+++ dv/uvm/core_ibex/scripts/compile-tb.py
@@ -7,10 +7,27 @@
 import argparse
 import os
 import sys
+import subprocess
 
 from ibex_cmd import get_compile_opts
 from scripts_lib import THIS_DIR, run_one, subst_vars
 from sim_cmd import get_simulator_cmd
+
+
+def _get_iss_pkgconfig_flags(specifiers, iss_pc, simulator):
+    _flags = subprocess.check_output(
+        args=(['pkg-config'] + specifiers + iss_pc),
+        universal_newlines=True,
+        ).strip()
+    if simulator == 'xlm':
+        # See xcelium documentation for the -Wld syntax for passing
+        # flags to the linker. Passing -rpath,<path> options is tricky
+        # because commas are parsed strangely between xrun and the xmsc
+        # tool, and its easy for the options to arrive malformed. Use
+        # the following hack to get it through.
+        if '-Wl' in _flags:  # This should be in LDFLAGS only
+            _flags = "'-Xlinker {}'".format(_flags.replace('-Wl,', ''))
+    return _flags
 
 
 def main() -> int:
@@ -36,6 +53,36 @@ def main() -> int:
 
     os.makedirs(args.output, exist_ok=True)
 
+    subst_vars_dict = {
+        'core_ibex': core_ibex,
+        'out': args.output,
+        'cmp_opts': get_compile_opts(args.ibex_config,
+                                     args.simulator)
+    }
+
+    # Find the correct flags for the tb to link against the compiled ISS
+    spike_iss_pc = ['riscv-riscv', 'riscv-disasm', 'riscv-fdt']
+    iss_pkgconfig_dict = {
+        'ISS_CFLAGS'  : ['--cflags'],
+        'ISS_LDFLAGS' : ['--libs-only-other'],
+        'ISS_LIBS'    : ['--libs-only-l', '--libs-only-L'],
+    }
+    if args.en_cosim:
+        try:
+            subprocess.check_output(['pkg-config', '--exists'] + spike_iss_pc)
+        except subprocess.CalledProcessError as err:
+            raise RuntimeError(
+                f'Failed to find {spike_iss_pc} pkg-config packages. '
+                f'Did you set the PKG_CONFIG_PATH correctly?') from err
+        subst_vars_dict.update(
+            {k: _get_iss_pkgconfig_flags(v,
+                                         spike_iss_pc,
+                                         args.simulator)
+             for k, v in iss_pkgconfig_dict.items()})
+
+    if args.en_cov:
+        subst_vars_dict.update({'shared_cov_dir': args.shared_cov_dir})
+
     enables = {
         'cov_opts': args.en_cov,
         'wave_opts': args.en_wave,
@@ -44,14 +91,7 @@ def main() -> int:
     compile_cmds, _ = get_simulator_cmd(args.simulator, enables)
 
     for pre_cmd in compile_cmds:
-        cmd = subst_vars(pre_cmd,
-                         {
-                             'core_ibex': core_ibex,
-                             'out': args.output,
-                             'shared_cov_dir': args.shared_cov_dir,
-                             'cmp_opts': get_compile_opts(args.ibex_config,
-                                                          args.simulator)
-                         })
+        cmd = subst_vars(pre_cmd, subst_vars_dict)
         retcode = run_one(args.verbose, ['sh', '-c', cmd],
                           redirect_stdstreams='/dev/null')
         if retcode:
