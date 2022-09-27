2d9caf6fac84eac05bef149b86fe330c8d1d08d3
Canberk Topal <ctopal@lowrisc.org>
2022-09-27 10:16:35 +0100

    [rtl] Change how we record debug causes
    This commit changes when we cath the debug causes. Since debug_cause_o
    only gets latched when `csr_save_cause_o` is high, it would work if
    we change the cause with a mux that is connected to the input signals.
    
    Resolves #1772
    
    Signed-off-by: Canberk Topal <ctopal@lowrisc.org>

--- rtl/ibex_controller.sv
+++ rtl/ibex_controller.sv
@@ -114,6 +114,7 @@ module ibex_controller #(
 
   logic nmi_mode_q, nmi_mode_d;
   logic debug_mode_q, debug_mode_d;
+  dbg_cause_e debug_cause_d, debug_cause_q;
   logic load_err_q, load_err_d;
   logic store_err_q, store_err_d;
   logic exc_req_q, exc_req_d;
@@ -417,6 +418,22 @@ module ibex_controller #(
 
   assign unused_irq_timer = irqs_i.irq_timer;
 
+  assign debug_cause_d = trigger_match_i   ? DBG_CAUSE_TRIGGER :
+                         ebreak_into_debug ? DBG_CAUSE_EBREAK  :
+                         debug_req_i       ? DBG_CAUSE_HALTREQ :
+                         do_single_step_d  ? DBG_CAUSE_STEP    :
+                                             DBG_CAUSE_NONE ;
+
+  always_ff @(posedge clk_i or negedge rst_ni) begin
+    if (!rst_ni) begin
+      debug_cause_q <= DBG_CAUSE_NONE;
+    end else begin
+      debug_cause_q <= debug_cause_d;
+    end
+  end
+
+  assign debug_cause_o = debug_cause_q;
+
   /////////////////////
   // Core controller //
   /////////////////////
@@ -453,7 +470,6 @@ module ibex_controller #(
     flush_id               = 1'b0;
 
     debug_csr_save_o       = 1'b0;
-    debug_cause_o          = DBG_CAUSE_EBREAK;
     debug_mode_d           = debug_mode_q;
     nmi_mode_d             = nmi_mode_q;
 
@@ -657,13 +673,6 @@ module ibex_controller #(
         debug_csr_save_o = 1'b1;
 
         csr_save_cause_o = 1'b1;
-        if (trigger_match_i) begin
-          debug_cause_o = DBG_CAUSE_TRIGGER;     // (priority 4)
-        end else if (debug_req_i) begin
-          debug_cause_o = DBG_CAUSE_HALTREQ;     // (priority 1)
-        end else begin
-          debug_cause_o = DBG_CAUSE_STEP;        // (priority 0, lowest)
-        end
 
         // enter debug mode
         debug_mode_d = 1'b1;
@@ -693,7 +702,6 @@ module ibex_controller #(
 
           // dcsr
           debug_csr_save_o = 1'b1;
-          debug_cause_o    = DBG_CAUSE_EBREAK;
         end
 
         // enter debug mode
