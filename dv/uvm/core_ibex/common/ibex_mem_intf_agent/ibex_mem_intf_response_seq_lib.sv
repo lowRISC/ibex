653b97e9e3863c5d878d5508fe9ebf8057b5d0e4
Canberk Topal <ctopal@lowrisc.org>
2022-10-16 03:30:03 +0100

    [dv] Don't inject mem errors at test_control_addr
    This will prevent seeing mismatches right at the end of our test.
    Before this change, mem_error_test could inject error at the store
    instruction in which we finish up the test, resulting with mismatches
    with Spike and Ibex on the instructions after finishing.
    
    Also do the same prevention for signature_addr as well, since we also
    don't want to corrupt that memory transaction too.
    
    Signed-off-by: Canberk Topal <ctopal@lowrisc.org>

--- dv/uvm/core_ibex/common/ibex_mem_intf_agent/ibex_mem_intf_response_seq_lib.sv
+++ dv/uvm/core_ibex/common/ibex_mem_intf_agent/ibex_mem_intf_response_seq_lib.sv
@@ -60,6 +60,11 @@ class ibex_mem_intf_response_seq extends uvm_sequence #(ibex_mem_intf_seq_item);
       enable_error = 1'b0;
       error_synch = 1'b1;
       aligned_addr = {req.addr[DATA_WIDTH-1:2], 2'b0};
+      // Do not inject any error to the handshake test_control_addr
+      // TODO: Parametrize this. Until then, this needs to be changed manually.
+      if (aligned_addr inside {32'h8ffffff8, 32'h8ffffffc}) begin
+        req.error = 1'b0;
+      end
       if (req.error) begin
         `DV_CHECK_STD_RANDOMIZE_FATAL(rand_data)
         req.data = rand_data;
