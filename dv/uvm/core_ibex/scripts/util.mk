afef45d78e34fd47030bfda9440ceb3833bf6e22
Harry Callahan <hcallahan@lowrisc.org>
2023-01-04 10:50:49 +0000

    Add missing copyright messages / licenses

--- dv/uvm/core_ibex/scripts/util.mk
+++ dv/uvm/core_ibex/scripts/util.mk
@@ -1,3 +1,7 @@
+# Copyright lowRISC contributors.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
 ###############################################################################
 ######## EXAMPLE OF VARIABLE DUMPING ############
 # This target depends on the vendored in code in $(GEN_DIR). It also depends on the
