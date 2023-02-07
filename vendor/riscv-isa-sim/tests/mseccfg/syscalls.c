744c06586f9c094e79afb41bbbae31e48006dd75
Saad Khalid <saad.khalid@lowrisc.org>
2023-02-07 14:24:56 +0000

    updated riscv-isa-sim fork

--- vendor/riscv-isa-sim/tests/mseccfg/syscalls.c
+++ vendor/riscv-isa-sim/tests/mseccfg/syscalls.c
@@ -10,6 +10,35 @@
 
 #define SYS_write 64
 
+// Ibex specific macros
+#define SIGNATURE_ADDR 0x8ffffff8
+
+// signatue type - should be stored at sign_addr[7:0]
+#define CORE_STATUS 0x0
+#define TEST_RESULT 0x1
+#define WRITE_GPR 0x2
+#define WRITE_CSR 0x3
+
+// core status - should be stored at sign_addr[12:8]
+#define INITIALIZED 0x0
+#define IN_DEBUG_MODE 0x1
+#define IN_MACHINE_MODE 0x2
+#define IN_HYPERVISOR_MODE 0x3
+#define IN_SUPERVISOR_MODE 0x4
+#define IN_USER_MODE 0x5
+#define HANDLING_IRQ 0x6
+#define FINISHED_IRQ 0x7
+#define HANDLING_EXCEPTION 0x8
+#define INSTR_FAULT_EXCEPTION 0x9
+#define ILLEGAL_INSTR_EXCEPTION 0xa
+#define LOAD_FAULT_EXCEPTION 0xb
+#define STORE_FAULT_EXCEPTION 0xc
+#define EBREAK_EXCEPTION 0xd
+
+// test result - should be stored at sign_addr[8]
+#define TEST_PASS 0x0
+#define TEST_FAIL 0x1
+
 #undef strcmp
 
 extern volatile uint64_t tohost;
@@ -55,7 +84,15 @@ void setStats(int enable)
 
 void __attribute__((noreturn)) tohost_exit(uintptr_t code)
 {
-  tohost = (code << 1) | 1;
+  asm volatile("fence;"
+               "sw %1, 0(%0);"
+               "sw %2, 0(%0);"
+               "2:;"
+               "j 2b;"
+              :
+              : "r"(SIGNATURE_ADDR), "r"((code << 8) | CORE_STATUS), "r"((TEST_PASS << 8) | TEST_RESULT)  /* Inputs */
+              : "memory");
+//  tohost = (code << 1) | 1;
   while (1);
 }
 
