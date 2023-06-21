a7845832a2895ac3be8e28f41b87992207daee30
Greg Chadwick <gac@lowrisc.org>
2023-06-21 12:17:18 +0100

    [dv,fcov] Add additional illegal bins to PMP fcov

--- dv/uvm/core_ibex/fcov/core_ibex_pmp_fcov_if.sv
+++ dv/uvm/core_ibex/fcov/core_ibex_pmp_fcov_if.sv
@@ -384,7 +384,8 @@ interface core_ibex_pmp_fcov_if import ibex_pkg::*; #(
           illegal_bins illegal_machine_deny_read =
             // Ensuring MML is high and we are in a R allowed configuration in Machine Mode
             (binsof(cp_region_priv_bits) intersect {MML_WRM_RU, MML_WRM_WRU, MML_RM_RU, MML_RM,
-                                                    MML_WRM, MML_XRM, MML_XRM_XU, NONE} &&
+                                                    MML_WRM, MML_XRM, MML_XRM_XU, NONE,
+                                                    W, X, XW} &&
              binsof(cp_priv_lvl_dside) intersect {PRIV_LVL_M} &&
              binsof(cp_req_type_dside) intersect {PMP_ACC_READ} &&
              binsof(pmp_dside_req_err) intersect {1});
@@ -428,7 +429,8 @@ interface core_ibex_pmp_fcov_if import ibex_pkg::*; #(
              binsof(pmp_dside_req_err) intersect {1});
           illegal_bins illegal_machine_deny_write =
             // Ensuring MML is high and we are in a W allowed configuration in Machine Mode
-            (binsof(cp_region_priv_bits) intersect {MML_WRM_WRU, MML_WRM_RU, MML_WRM, NONE} &&
+            (binsof(cp_region_priv_bits) intersect {MML_WRM_WRU, MML_WRM_RU, MML_WRM, NONE,
+                                                    R, X, XR, XW} &&
              binsof(cp_priv_lvl_dside) intersect {PRIV_LVL_M} &&
              binsof(cp_req_type_dside) intersect {PMP_ACC_WRITE} &&
              binsof(pmp_dside_req_err) intersect {1});
