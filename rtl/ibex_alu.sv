51dd473ab71044e3fc43386c06276790a940c08a
Michael Munday <mike.munday@lowrisc.org>
2024-04-15 15:34:51 +0100

    DO NOT MERGE
    Signed-off-by: Michael Munday <mike.munday@lowrisc.org>

--- rtl/ibex_alu.sv
+++ rtl/ibex_alu.sv
@@ -3,6 +3,8 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
+// Dummy change to force rebuild.
+
 /**
  * Arithmetic logic unit
  */
