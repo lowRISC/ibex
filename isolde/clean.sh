f49a7d0fce552643a39b5362ff32a43bb1b5fb11
darotsr <daro@tsr>
2025-02-26 15:16:08 +0200

    updates in doc

---
+++ isolde/clean.sh
@@ -0,0 +1,9 @@
+rm -fr lca_system/bin/tb_lca_system
+rm -fr lca_system/sim/core/tb_lca_system_*
+rm -fr lca_system/sw/bin
+rm -fr lca_system/sw/build
+#
+rm -fr tca_system/bin/tb_lca_system
+rm -fr tca_system/sim/core/tb_lca_system_*
+rm -fr tca_system/sw/bin
+rm -fr tca_system/sw/build
--- isolde/regression.sh
+++ isolde/regression.sh
@@ -3,8 +3,9 @@
 # Iterate i from 0 to 10
 for i in {0..10}; do
   echo "Running test with IMEM_LATENCY=$i"
-  make -C tca_system IMEM_LATENCY=$i PE=redmule TEST=redmule_complex_32b   verilate test-build veri-run
-  make -C tca_system IMEM_LATENCY=$i PE=redmule TEST=redmule_complex_128b  verilate test-build veri-run
+  make -C tca_system IMEM_LATENCY=$i PE=redmule TEST=redmule_complex_32b     verilate test-build veri-run
+  make -C tca_system IMEM_LATENCY=$i PE=redmule TEST=redmule_complex_128b    verilate test-build veri-run
+  make -C tca_system IMEM_LATENCY=$i PE=redmule TEST=redmule_complex_ex_reg  verilate test-build veri-run
 
   # Check if the command succeeded
   if [ $? -ne 0 ]; then
