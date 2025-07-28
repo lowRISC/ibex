8594c83eb086cd5281b44a029ecdeda036365cf9
Gary Guo <gary.guo@lowrisc.org>
2025-07-28 14:17:27 +0100

    [formal] update sail
    This updates sail to a commit that is compatible with OCaml 5.0.
    The updated sail also include a commit to include unreachable_loc already
    so related parts is dropped from buildspec.py.
    
    Signed-off-by: Gary Guo <gary.guo@lowrisc.org>

--- dv/formal/buildspec.py
+++ dv/formal/buildspec.py
@@ -98,14 +98,8 @@ if len(sys.argv) > 1:
         with open("build/ibexspec.sv", "r") as f:
             c = f.read()
         c = c.replace(
-            "sail_reached_unreachable = 1;",
-            "begin sail_reached_unreachable = 1; sail_reached_unreachable_loc = `__LINE__; end"
-        ).replace(
             "sail_reached_unreachable = 0;",
             "begin sail_reached_unreachable = 0; sail_reached_unreachable_loc = -1; end"
-        ).replace(
-            "bit sail_reached_unreachable;",
-            "bit sail_reached_unreachable;\n\tlogic [31:0] sail_reached_unreachable_loc;"
         )
         with open("build/ibexspec.sv", "w") as f:
             f.write(c)
