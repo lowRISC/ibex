36f327e9b1f6b3d5d3fecdf5a27f76c7674e8903
Tim Hutt <tdhutt@gmail.com>
2025-11-03 13:26:54 +0000

    Add missing zicsr to -march in example Makefile
    Newer GCC toolchains require you to explicitly enable zicsr otherwise you get errors when trying to compile code with CSR access instructions.

--- examples/sw/simple_system/common/common.mk
+++ examples/sw/simple_system/common/common.mk
@@ -7,8 +7,8 @@ COMMON_DIR := $(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
 COMMON_SRCS = $(wildcard $(COMMON_DIR)/*.c)
 INCS := -I$(COMMON_DIR)
 
-# ARCH = rv32im # to disable compressed instructions
-ARCH ?= rv32imc
+# ARCH = rv32im_zicsr # to disable compressed instructions
+ARCH ?= rv32imc_zicsr
 
 ifdef PROGRAM
 PROGRAM_C := $(PROGRAM).c
